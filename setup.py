8ee49c4398482eb72fbc33c124c36125650caa86
jmmauricio <jmmauricio6@gmail.com>
2015-03-20 12:32:24 +0100

    new commit

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ from setuptools import setup, find_packages
 def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
-version = "0.0.3c"
+version = "0.0.3d"
 
 setup(
     name = "pypstools",
